4504ee02f8637ca37fdbc7e3c0fa71479553a37a
Peter-JanGootzen <PeterJan.Gootzen@gmail.com>
2018-02-15 22:34:53 +0100

    Added travis and heroku

--- database/seeds/ToolsTableSeeder.php
+++ database/seeds/ToolsTableSeeder.php
@@ -11,7 +11,7 @@ class ToolsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Tool::truncate();
         DB::table('tools')->insert([
             'name' => 'Heroku'
         ]);
