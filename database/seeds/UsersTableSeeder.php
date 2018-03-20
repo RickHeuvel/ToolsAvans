1f776fcd346477d41c529db871669944bff91a9e
Peter-JanGootzen <PeterJan.Gootzen@gmail.com>
2018-03-20 22:41:32 +0100

    Added every SO teammembers avans user profile to the users seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,7 +22,17 @@ class UsersTableSeeder extends Seeder
             'location' => 'OWB',
             'role' => 'admin'
         ]);
-
+        DB::table('users')->insert([
+            'name' => 'pjmgootz',
+            'email' => 'pjm.gootzen@student.avans.nl',
+            'provider' => 'avans',
+            'provider_id' => 'pjmgootz',
+            'nickname' => 'Peter-Jan Gootzen',
+            'firstName' => 'Peter-Jan',
+            'lastName' => 'Gootzen',
+            'location' => 'OWB',
+            'role' => 'admin'
+        ]);
         DB::table('users')->insert([
             'name' => 'magweter',
             'email' => 'mag.vandewetering@student.avans.nl',
@@ -31,6 +41,50 @@ class UsersTableSeeder extends Seeder
             'nickname' => 'Martijn van de Wetering',
             'firstName' => 'Martijn',
             'lastName' => 'van de Wetering',
+            'location' => 'OWB',
+            'role' => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'kmthulst',
+            'email' => 'kmt.vanderhulst@student.avans.nl',
+            'provider' => 'avans',
+            'provider_id' => 'kmthulst',
+            'nickname' => 'Koen van der Hulst',
+            'firstName' => 'Koen',
+            'lastName' => 'van der Hulst',
+            'location' => 'OWB',
+            'role' => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'rheuvel12',
+            'email' => 'r.vandenheuvel12@student.avans.nl',
+            'provider' => 'avans',
+            'provider_id' => 'rheuvel12',
+            'nickname' => 'Rick van den Heuvel',
+            'firstName' => 'Rick',
+            'lastName' => 'van den Heuvel',
+            'location' => 'OWB',
+            'role' => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'bbmeerlo',
+            'email' => 'bb.meerlo@student.avans.nl',
+            'provider' => 'avans',
+            'provider_id' => 'bbmeerlo',
+            'nickname' => 'Bram-Boris Meerlo',
+            'firstName' => 'Bram-Boris',
+            'lastName' => 'Meerlo',
+            'location' => 'OWB',
+            'role' => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'jbscholl',
+            'email' => 'jb.schollaert@student.avans.nl',
+            'provider' => 'avans',
+            'provider_id' => 'jbscholl',
+            'nickname' => 'Jan Schollaert',
+            'firstName' => 'Jan',
+            'lastName' => 'Schollaert',
             'location' => 'OWB',
             'role' => 'admin'
         ]);
