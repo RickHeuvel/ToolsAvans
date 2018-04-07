9ff414215f8129cba64c4d7d5ba4d5e46a3bf670
Bram-Boris Meerlo <bramboris@outlook.com>
2018-04-07 13:20:17 +0200

    Added Feedback tests (#62)
    * Added feedback tests
    
    * Fixed rejected assertion

---
+++ tests/unit/Controllers/JudgingControllerTest.php
@@ -0,0 +1,173 @@
+<?php
+namespace tests\unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ToolController;
+use App\Http\Controllers\JudgingController;
+use App\User;
+use App\Tool;
+use App\ToolStatus;
+use Auth;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class JudgingControllerTest extends TestCase
+{
+    /**
+     * Test approveTool()
+     *
+     * @return void
+     */
+    public function testApproveTool()
+    {
+        $auth = new AuthController();
+        $toolController = new ToolController();
+        $judgingController = new JudgingController();
+        Storage::fake('tool-images');
+
+        $user = factory(User::class)->states('student')->make();
+        $auth->login($user);
+
+        $toolname = 'testName';
+        $request = Request::create(
+            'tools',
+            'POST',
+            [
+                'name'          => $toolname,
+                'description'   => 'test description',
+                'category'      => 'Website',
+                'status'        => 'concept',
+                'url'           => 'https://www.testWebsite.nl',
+            ],
+            [],
+            [
+                'logo'          => UploadedFile::fake()->image('logo.png'),
+                'images'        => [
+                    UploadedFile::fake()->image('image-1.png'),
+                    UploadedFile::fake()->image('image-2.png'),
+                    UploadedFile::fake()->image('image-3.png')
+                ],
+            ]
+        );
+        $toolController->store($request);
+        
+        $tool = Tool::where('slug', Str::slug($toolname))->first();
+        $this->assertTrue($tool->status()->first()->isConcept());
+
+
+        $judgingController->approveTool(Str::slug($toolname));
+
+        $tool = Tool::where('slug', Str::slug($toolname))->first();
+        $this->assertTrue($tool->status()->first()->isActive());
+
+    }
+
+    /**
+     * Test test rejectTool()
+     *
+     * @return void
+     */
+    public function testRejectTool()
+    {
+        $auth = new AuthController();
+        $toolController = new ToolController();
+        $judgingController = new JudgingController();
+        Storage::fake('tool-images');
+
+        $user = factory(User::class)->states('student')->make();
+        $auth->login($user);
+
+        $toolname = 'testName';
+        $request = Request::create(
+            'tools',
+            'POST',
+            [
+                'name'          => $toolname,
+                'description'   => 'test description',
+                'category'      => 'Website',
+                'status'        => 'concept',
+                'url'           => 'https://www.testWebsite.nl',
+            ],
+            [],
+            [
+                'logo'          => UploadedFile::fake()->image('logo.png'),
+                'images'        => [
+                    UploadedFile::fake()->image('image-1.png'),
+                    UploadedFile::fake()->image('image-2.png'),
+                    UploadedFile::fake()->image('image-3.png')
+                ],
+            ]
+        );
+        $toolController->store($request);
+        
+        $tool = Tool::where('slug', Str::slug($toolname))->first();
+        $this->assertTrue($tool->status()->first()->isConcept());
+
+
+        $judgingController->rejectTool(Str::slug($toolname));
+
+        $tool = Tool::where('slug', Str::slug($toolname))->first();
+        $this->assertTrue($tool->status()->first()->isRejected());
+
+    }
+
+    /**
+     * Test requestChanges()
+     *
+     * @return void
+     */
+    public function testRequestToolChanges()
+    {
+        $auth = new AuthController();
+        $toolController = new ToolController();
+        $judgingController = new JudgingController();
+        Storage::fake('tool-images');
+
+        $user = factory(User::class)->states('student')->make();
+        $auth->login($user);
+
+        $toolname = 'testName';
+        $feedback = 'test feedback';
+        $request = Request::create(
+            'tools',
+            'POST',
+            [
+                'name'          => $toolname,
+                'description'   => 'test description',
+                'category'      => 'Website',
+                'status'        => 'concept',
+                'url'           => 'https://www.testWebsite.nl',
+            ],
+            [],
+            [
+                'logo'          => UploadedFile::fake()->image('logo.png'),
+                'images'        => [
+                    UploadedFile::fake()->image('image-1.png'),
+                    UploadedFile::fake()->image('image-2.png'),
+                    UploadedFile::fake()->image('image-3.png')
+                ],
+            ]
+        );
+        $toolController->store($request);
+        
+        $request = Request::create(
+            'tools.requestToolChanges',
+            'POST',
+            [
+                'tool_slug'     => Str::slug($toolname),
+                'feedback'      => $feedback,    
+            ]
+        );
+
+        $judgingController->requestToolChanges($request,Str::slug($toolname));
+
+        $tool = Tool::where('slug', Str::slug($toolname))->first();
+        $this->assertTrue($tool->feedback()->first()->feedback == $feedback);
+
+    }
+
+    
+}
