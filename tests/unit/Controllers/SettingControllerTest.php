e9937ee6c11ac661317ec22bc23b6cb9b385c3e3
Rick van den Heuvel <Rick.heuvel@outlook.com>
2018-05-05 13:56:02 +0200

    unittest settingscontroller added (#95)
    * unittest settingscontroller added
    
    * Update SettingControllerTest.php

---
+++ tests/unit/Controllers/SettingControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Tests\unit\Controllers;
+
+use App\Http\Controllers\SettingController;
+use Tests\TestCase;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use App\User;
+
+class SettingControllerTest extends TestCase
+{
+
+    public function testUpdateConceptMail()
+    {
+        $auth = new AuthController();
+        $controller = new SettingController();
+        $user = factory(User::class)->states('admin')->make();
+        $auth->login($user);
+
+        $request = Request::create(
+            'settings',
+            'PUT',
+            [
+                'settings' => [
+                    'conceptmailfrequence' => 'Weekly',
+                    'conceptmailday' => 'Tuesday',
+                    'conceptmailtime' => '20:30:00',
+                ]
+            ]
+        );
+        $controller->updateConceptMail($request);
+
+        $this->assertDatabaseHas('settings', [
+            'name' => 'conceptmailfrequence',
+            'val' => 'Weekly',
+            'name' => 'conceptmailday',
+            'val' => 'Tuesday',
+            'name' => 'conceptmailtime',
+            'val' => '20:30:00',
+        ]);
+    }
+}
